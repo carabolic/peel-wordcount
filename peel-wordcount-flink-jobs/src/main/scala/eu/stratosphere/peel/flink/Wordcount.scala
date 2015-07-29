d0a76136f9e3cb7cfb80269871402277e23f33ce
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2015-07-29 18:29:54 +0200

    Aligned style in both Wordcount jobs.

--- peel-wordcount-flink-jobs/src/main/scala/eu/stratosphere/peel/flink/Wordcount.scala
+++ peel-wordcount-flink-jobs/src/main/scala/eu/stratosphere/peel/flink/Wordcount.scala
@@ -2,16 +2,13 @@ package eu.stratosphere.peel.flink
 
 import org.apache.flink.api.scala._
 
-/**
- * Example Apache Flink Job that counts the word occurrences in a given dataset.
- *
- * This example is built on top of the Apache Flink Scala API.
- */
+/** A `WordCount` workload job for Flink. */
 object Wordcount {
 
-  def main (args: Array[String]) {
+  def main(args: Array[String]) {
     if (args.length != 2) {
-      return
+      Console.err.println("Usage: <jar> inputPath outputPath")
+      System.exit(-1)
     }
 
     val inputPath = args(0)
@@ -19,13 +16,13 @@ object Wordcount {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
     env.readTextFile(inputPath)
-      .flatMap { _.toLowerCase.split("\\W+") }
-      .map { (_, 1) }
+      .flatMap(_.toLowerCase.split("\\W+"))
+      .map((_, 1))
       .groupBy(0)
       .sum(1)
       .writeAsCsv(outputPath)
 
-    env.execute()
+    env.execute("peel-wordcount-flink")
   }
 
 }
--- peel-wordcount-spark-jobs/src/main/scala/eu/stratosphere/peel/spark/Wordcount.scala
+++ peel-wordcount-spark-jobs/src/main/scala/eu/stratosphere/peel/spark/Wordcount.scala
@@ -2,24 +2,24 @@ package eu.stratosphere.peel.spark
 
 import org.apache.spark.{SparkContext, SparkConf}
 
-/**
- * Created by carabolic on 15/07/15.
- */
+/** A `WordCount` workload job for Spark. */
 object Wordcount {
 
   def main(args: Array[String]) {
     if (args.length != 2) {
-      return
+      Console.err.println("Usage: <jar> inputPath outputPath")
+      System.exit(-1)
     }
 
     val inputPath = args(0)
     val outputPath = args(1)
 
-    val spark = new SparkContext()
+    val spark = new SparkContext(new SparkConf().setAppName("peel-wordcount-spark"))
     spark.textFile(inputPath)
-      .flatMap { _.toLowerCase.split("\\W+") }
-      .map { (_, 1) }
-      .reduceByKey(_ + _).saveAsTextFile(outputPath)
+      .flatMap(_.toLowerCase.split("\\W+"))
+      .map((_, 1))
+      .reduceByKey(_ + _)
+      .saveAsTextFile(outputPath)
   }
 
 }
